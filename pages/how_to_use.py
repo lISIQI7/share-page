3bd2c716485edae086b1fbfe8a73f2f72ee573bc
LISIQI7 <128582132+lISIQI7@users.noreply.github.com>
2023-05-13 14:19:25 +0800

    how_to_use.py

--- pages/how_to_use.py
+++ pages/how_to_use.py
@@ -14,24 +14,23 @@ Fascinating tool to convert images into pixel art!\n
 Playground  [open it](https://tensorspace.org/html/playground/lenet.html) 
 
 
-# Basic functions
+# basic functions
+
 ## colorpallet
 This site converts colors.  
 Select the color palette to use when converting colors.  
 Pyxel color  [Try it](https://sophisticated-palette.streamlit.app/).  
 
-
 ## ratio
+
 This is a slider that can be adjusted in increments of 0.01. The lower the number, the larger the dot.
 
-
 ## Custom Pallet
+
 You can create your own ColorPallet.  
-Enter the colors you want to add to the palette in the table using color codes.  
-The colors entered in the table will be displayed on the right side.  
 
  
 # More Options
-Return to akaz [👍](https://pixelart.streamlit.app/how_to_use)
+Return to akaz [📨](https://pixelart.streamlit.app/how_to_use)
  
 """)
