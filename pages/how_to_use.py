f6bbd4725f8935fda449dc9f3f5d4cbcd2a1a2c6
LISIQI7 <128582132+lISIQI7@users.noreply.github.com>
2023-05-13 14:30:51 +0800

    how_to_use.py

--- pages/how_to_use.py
+++ pages/how_to_use.py
@@ -16,16 +16,16 @@ Playground  [open it](https://tensorspace.org/html/playground/lenet.html)
 
 # Basic functions
 
-### 1️⃣colorpallet
+### ✨colorpallet
 This site converts colors.\n  
 Select the color palette to use when converting colors.\n  
 Pyxel color  [Try it](https://sophisticated-palette.streamlit.app/).  
 
-### 2️⃣ratio
+### ✨ratio
 
 This is a slider that can be adjusted in increments of 0.01. The lower the number, the larger the dot.
 
-### 3️⃣custom pallet
+### ✨custom pallet
 
 You can create your own ColorPallet.  
  
