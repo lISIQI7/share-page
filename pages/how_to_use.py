db862535362d6575e3019ef72564c989f264a821
LISIQI7 <128582132+lISIQI7@users.noreply.github.com>
2023-05-13 14:26:41 +0800

    how_to_use.py

--- pages/how_to_use.py
+++ pages/how_to_use.py
@@ -7,14 +7,14 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 st.markdown("""
-## share-page
+# Share-page
 Language : [English]\n
 LICENSE  : Modifications copyright (C) 2023 AKAZ and LISIQI7\n
 Fascinating tool to convert images into pixel art!\n
 Playground  [open it](https://tensorspace.org/html/playground/lenet.html) 
 
 
-## basic functions
+# Basic functions
 
 ### colorpallet
 This site converts colors.  
@@ -29,7 +29,7 @@ This is a slider that can be adjusted in increments of 0.01. The lower the numbe
 
 You can create your own ColorPallet.  
  
-## more options
+# More options
 Return to akaz [📨](https://pixelart.streamlit.app/how_to_use)
  
 """)
